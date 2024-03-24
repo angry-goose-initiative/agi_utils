dfe8e72aae418b185336d295c6bab44e4e10aeec
brainyshadow <seb997a@hotmail.com>
2024-03-24 16:59:23 +0100

    Changed return type

--- vserial/vserial.cc
+++ vserial/vserial.cc
@@ -1,20 +1,19 @@
 #include "vserial.h"
-#include <fcntl.h>
-#include <iostream>
-#include <unistd.h>
-#include <string>
-#include <optional>
 
-using namespace agi::vserial;
 
-std::optional<std::string> getFileName(){
-        int pt;
+
+agi::vserial::Res agi::vserial::create_pseudo_serial(){
+    int pt;
+    
+    agi::vserial::Res return_value;
 
     pt = open("/dev/ptmx", O_RDWR | O_NOCTTY);
     if (pt < 0)
     {
         perror("open /dev/ptmx");
-        return std::nullopt;
+        return_value.file_path = std::nullopt;
+        return_value.fd = std::nullopt;
+        return return_value;
     }
 
     grantpt(pt);
@@ -25,9 +24,12 @@ std::optional<std::string> getFileName(){
     // Check if ptsname returned a valid string
     if (pts_name == nullptr) {
         // Return an empty string if ptsname failed
-        return std::nullopt;
+        return_value.file_path = std::nullopt;
+        return_value.fd = std::nullopt;
     }
 
     // Convert the C-style string to a C++ std::string
-    return std::string(pts_name);
+    return_value.file_path = std::string(pts_name);
+    return_value.fd = pt;
+    return return_value;
 }
--- vserial/vserial.h
+++ vserial/vserial.h
@@ -1,10 +1,23 @@
 #pragma once
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
 #include <string>
 #include <optional>
 
 
 namespace agi::vserial {
-    std::optional<std::string> getFileName();
 
+    /**
+     * @brief A function to open a pseudo terminal.
+     * 
+     * @return Struct containing the fd and the filePath
+     */
 
+    struct Res {
+        std::optional<std::string> file_path;
+        std::optional<int> fd;
+    };
+
+    Res create_pseudo_serial();
 } // namespace agi::vserial
