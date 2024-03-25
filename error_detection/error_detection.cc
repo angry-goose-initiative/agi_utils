4a2b7c149af749af4f3b206cea1d8206cad21b26
Sam Graham <77069977+samgraham03@users.noreply.github.com>
2024-03-25 19:47:55 -0400

    Add CRC-8 checksum calculator (#2)
    * Add CRC-8 checksum calculator
    
    Bug fixes
    
    Comment
    
    * Address code review comments

---
+++ error_detection/error_detection.cc
@@ -0,0 +1,21 @@
+#include "error_detection.h"
+
+uint8_t agi::error_detection::crc8(const uint8_t* data, std::size_t length) {
+    constexpr uint8_t CRC8_POLYNOMIAL = 0x07; // CRC-8 Generator polynomial
+    constexpr uint8_t CRC8_MSB = (1U << 7U); // Most significat bit of byte
+
+    uint8_t crc = 0x00;
+
+    for (std::size_t i = 0; i < length; i++) {
+        crc ^= data[i];
+        for (int b = 0; b < 8; b++) {
+            if (crc & CRC8_MSB) {
+                crc = (crc << 1U) ^ CRC8_POLYNOMIAL;
+            } else {
+                crc <<= 1U;
+            }
+        }
+    }
+
+    return crc;
+}
---
+++ error_detection/error_detection.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+namespace agi::error_detection {
+
+/**
+ * @brief Compute 8 bit checksum on data buffer using polynomial division based CRC-8 algorithm
+ * 
+ * @param data, length
+ * @return 8 bit CRC checksum
+ */
+uint8_t crc8(const uint8_t* data, std::size_t length);
+
+} // namespace agi::error_detection
