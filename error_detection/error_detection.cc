b233ecbccc0488418fa3587a0c26dce355441e44
nickchan2 <thenicholaschan@gmail.com>
2024-03-29 01:01:27 -0400

    Change interface for agi::error_detection::crc8 and fix linting errors

--- error_detection/error_detection.cc
+++ error_detection/error_detection.cc
@@ -5,17 +5,19 @@
 
 #include "error_detection.h"
 
-uint8_t agi::error_detection::crc8(const uint8_t* data, std::size_t length) {
+#include <climits>
+
+uint8_t agi::error_detection::crc8(std::span<uint8_t> data) {
     constexpr uint8_t CRC8_POLYNOMIAL = 0x07; // CRC-8 Generator polynomial
     constexpr uint8_t CRC8_MSB = (1U << 7U); // Most significat bit of byte
 
     uint8_t crc = 0x00;
 
-    for (std::size_t i = 0; i < length; i++) {
-        crc ^= data[i];
-        for (int b = 0; b < 8; b++) {
+    for (const uint8_t val : data) {
+        crc ^= val;
+        for (int b = 0; b < CHAR_BIT; b++) {
             if (crc & CRC8_MSB) {
-                crc = (crc << 1U) ^ CRC8_POLYNOMIAL;
+                crc = (crc << 1U) ^ CRC8_POLYNOMIAL; // NOLINT(*-signed-bitwise)
             } else {
                 crc <<= 1U;
             }
