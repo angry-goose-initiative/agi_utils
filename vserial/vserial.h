5ec567ba9a30d7f14f4d501cdfff878b5fd63033
nickchan2 <thenicholaschan@gmail.com>
2025-01-18 12:25:31 -0500

    vserial tweaks

--- vserial/vserial.h
+++ vserial/vserial.h
@@ -4,33 +4,36 @@
 */
 
 #pragma once
-#include <fcntl.h>
-#include <iostream>
-#include <unistd.h>
+
 #include <string>
 #include <variant>
 
 namespace agi::vserial {
 
-    /**
-     * @brief A function to open a pseudo terminal.
-     * 
-     * @return Struct containing the fd and the filePath
-     */
+/**
+ * @brief A function to open a pseudo terminal.
+ * 
+ * @return Struct containing the fd and the file path
+ */
 
-    struct Res {
-        std::string file_path;
-        int fd;
+struct PtInfo {
+    std::string file_path;
+    int fd;
+};
+
+struct Error {
+    enum class SysCall {
+        OPEN,
+        GRANTPT,
+        UNLOCKPT,
+        PTSNAME
     };
+    SysCall sys_call;
+    int     errno_val;
+};
 
+using Res = std::variant<agi::vserial::PtInfo, agi::vserial::Error>;
 
-    struct Error_Res {
-        int open_pt = 0;
-        int grantpt = 0;
-        int unlockpt = 0;
-        bool ptsname = false;
-    };
+Res create_pseudo_serial();
 
-
-    std::variant<agi::vserial::Res, agi::vserial::Error_Res> create_pseudo_serial();
 } // namespace agi::vserial
